c4b1ffc9eeff8bfb748850db10373e395dde5478
diego <guatemaladiegojose@gmail.com>
2022-10-19 16:43:03 -0400

    commit tree c1879062d7e0acca69e8426feaf2f6d3bd753a01

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+const num1= 2;
+const num2= 31;
+const multiply = num1* num2;
+
+const random = Math.floor(Math.random()*10);
+
+const num3=19;
+const num4=5;
+const mod= num3 % num4;
+
+const max = Math.max(1,2,3,4,5,6,7,8,20);
+
+
+
+
+
+
